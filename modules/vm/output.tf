ca1d1d19a1f1df7fd374aa5df2e4575af30fafef
bri <284789+b-@users.noreply.github.com>
2025-08-17 23:12:39 -0400

    add more vm outputs

--- modules/vm/output.tf
+++ modules/vm/output.tf
@@ -18,3 +18,16 @@ output "mac_addresses" {
 output "creation_date" {
   value = time_static.creation_date.rfc3339
 }
+
+output "user_data" {
+  value = try(module.cloud_init_files[0].user_data[0].source_raw, null)
+}
+output "vendor_data" {
+  value = try(module.cloud_init_files[0].vendor_data[0].source_raw, null)
+}
+output "network_data" {
+  value = try(module.cloud_init_files[0].network_data[0].source_raw, null)
+}
+output "meta_data" {
+  value = try(module.cloud_init_files[0].meta_data[0].source_raw, null)
+}
